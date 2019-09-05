ed27c32f18a371f1c79f8d10e8807837dee05883
Santiago <sssilvar@unal.edu.co>
2019-09-05 18:02:34 -0500

    Update gitignore

---
+++ preprocessing/recon-all.py
@@ -0,0 +1,12 @@
+#!/bin/env python3
+import os
+import pandas as pd
+from os.path import dirname, realpath, join
+
+root = dirname(dirname(realpath(__file__)))
+
+if __name__ == '__main__':
+    # Data files
+    subjects_csv = join(root, 'data/subjects.csv')
+    # TODO: You know
+    # df =
