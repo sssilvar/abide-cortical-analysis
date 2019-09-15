3a58b423de30df82342949d4aaae8d26790cf252
Santiago Smith <sssilvar@unal.edu.co>
2019-09-15 14:10:05 -0500

    Multicore processing

--- preprocessing/stats_to_csv.sh
+++ preprocessing/stats_to_csv.sh
@@ -8,19 +8,26 @@ DATA_CSV="${ROOT}/data/subjects.csv"
 
 SUBJECTS=` awk -F";" '{ print $1 }' ${DATA_CSV}`
 SCRIPT='python2 /usr/local/freesurfer/bin/aparcstats2table'
-FEATURES_FILE="${SUBJECTS_DIR}/cortical_features.csv"
+
 
 # Parse stats to csv
 # Documentation: https://surfer.nmr.mgh.harvard.edu/fswiki/aparcstats2table
 # Example:
 #     aparcstats2table --subjects bert ernie fred margaret --hemi rh --meas thickness --tablefile aparc_stats.txt
 
-CMD="
-${SCRIPT} --subjects ${SUBJECTS}
---hemi lh rh
---meas thickness meancurv
---delimiter comma
---skip
---tablefile ${FEATURES_FILE}
-"
-echo ${CMD}
+for hemi in "lh" "rh"
+do
+    for meas in "thickness" "meancurv"
+    do
+        FEATURES_FILE="${SUBJECTS_DIR}/cortical_${meas}_${hemi}.csv"
+        CMD="
+        ${SCRIPT} --subjects ${SUBJECTS}
+        --hemi ${hemi}
+        --meas ${meas}
+        --delimiter comma
+        --skip
+        --tablefile ${FEATURES_FILE}
+        "
+        echo ${CMD}
+    done
+done
